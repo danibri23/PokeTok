1275453b4d014f456e7604b5fb6256cf126f4a3f
danibri23 <carlosbrizuelamereles@gmail.com>
2024-05-06 20:35:59 -0400

    feat: Add shared preferences service to PokemonRepository
    The code changes include adding the `SharedPreferencesService` dependency to the `PokemonRepository` implementation. This allows the repository to retrieve saved pokemons from shared preferences.

--- lib/core/dependency_injection/locator.dart
+++ lib/core/dependency_injection/locator.dart
@@ -1,5 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:poketok/core/services/http_client.dart';
+import 'package:poketok/core/services/shared_preferences_service.dart';
 import 'package:poketok/data/repositories/pokemon_repository_implementation.dart';
 import 'package:poketok/domain/repositories/pokemon_repository.dart';
 
@@ -9,10 +10,14 @@ void setupLocator() {
   locator.registerLazySingleton<HttpService>(
     () => HttpService(),
   );
+  locator.registerLazySingleton<SharedPreferencesService>(
+    () => SharedPreferencesService(),
+  );
 
   locator.registerLazySingleton<PokemonRepository>(
     () => PokemonRepositoryImplementation(
       httpService: locator<HttpService>(),
+      sharedPreferencesService: locator<SharedPreferencesService>(),
     ),
   );
 }
--- lib/data/repositories/pokemon_repository_implementation.dart
+++ lib/data/repositories/pokemon_repository_implementation.dart
@@ -1,20 +1,33 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:poketok/core/services/http_client.dart';
+import 'package:poketok/core/services/shared_preferences_service.dart';
 import 'package:poketok/domain/models/pokemon_model.dart';
 import 'package:poketok/domain/repositories/pokemon_repository.dart';
 
 class PokemonRepositoryImplementation implements PokemonRepository {
   final HttpService _httpService;
+  final SharedPreferencesService _sharedPreferencesService;
 
-  PokemonRepositoryImplementation({
-    required HttpService httpService,
-  }) : _httpService = httpService;
+  PokemonRepositoryImplementation(
+      {required HttpService httpService,
+      required SharedPreferencesService sharedPreferencesService})
+      : _httpService = httpService,
+        _sharedPreferencesService = sharedPreferencesService;
 
   @override
   Future<Pokemon> getPokemons(int pokemonId) async {
     final response = await _httpService.dio.get('/pokemon/$pokemonId');
     // log('Response: ${response.data}');
     return Pokemon.fromJson(response.data);
+  }
+
+  @override
+  Future<List<Pokemon?>> getSavedPokemons() async {
+    final savedPokemonsString =
+        await _sharedPreferencesService.getSavedPokemons();
+    final savedPokemonsJson = savedPokemonsString.map((e) => jsonDecode(e));
+    return savedPokemonsJson.map((e) => Pokemon.fromLocalJson(e)).toList();
   }
 }
