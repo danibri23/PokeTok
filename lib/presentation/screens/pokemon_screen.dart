e191c9e3258e7004ae4db24c0b3fca79b0db959c
danibri23 <carlosbrizuelamereles@gmail.com>
2024-05-07 20:56:58 -0400

    feat: Add lazy loading for images and improve favorite Pokemon functionality

--- lib/presentation/screens/pokemon_screen.dart
+++ lib/presentation/screens/pokemon_screen.dart
@@ -42,20 +42,10 @@ class _PokemonScreenState extends ConsumerState<PokemonScreen> {
   @override
   Widget build(BuildContext context) {
     final pokemonProvider = ref.watch(pokemonsProvider).pokemonList;
+    // log(pokemonProvider[7].types[0].typeName);
 
     return Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        title: const Text('Pokemons'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.of(context).pushNamed(FavoritesScreen.name);
-            },
-          ),
-        ],
-      ),
+      backgroundColor: Colors.green,
       body: pokemonProvider.isEmpty
           ? const Center(
               child: CircularProgressIndicator(),
@@ -77,24 +67,55 @@ class _PokemonScreenState extends ConsumerState<PokemonScreen> {
                     final pokemon = pokemonProvider[index];
                     return Column(
                       children: [
+                        const PokemonHeader(),
                         PokemonTitle(pokemon: pokemon),
-                        PokemonImage(pokemon: pokemon),
-                        ElevatedButton(
-                          onPressed: () {
-                            ref
-                                .read(pokemonsProvider.notifier)
-                                .addFavoritePokemon(pokemon);
-                          },
-                          child: const Text(
-                            'Add to favorites',
-                          ),
-                        ),
+                        Stack(
+                          alignment: Alignment.center,
+                          children: [
+                            ContainerStats(pokemon: pokemon),
+                            Positioned(
+                              bottom: 0,
+                              child: PokemonImage(pokemon: pokemon),
+                            ),
+                          ],
+                        )
                       ],
                     );
                   },
                 ),
               ),
             ),
+    );
+  }
+}
+
+class PokemonHeader extends StatelessWidget {
+  const PokemonHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.green,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const Text(
+            'Mis Favoritos',
+            style: TextStyle(
+              fontSize: 20,
+              letterSpacing: -1.0,
+              color: Colors.white,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, FavoritesScreen.name);
+            },
+            icon: const Icon(Icons.favorite_border,
+                size: 28, color: Colors.white),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -109,7 +130,7 @@ class PokemonImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(pokemon.sprite);
+    return Image.network(pokemon.sprite, height: 200, width: 200);
   }
 }
 
@@ -124,7 +145,257 @@ class PokemonTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(pokemon.name),
+      title: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Pokemon nro ${pokemon.id}',
+            style: const TextStyle(color: Colors.white),
+          ),
+          Text(
+              '${pokemon.name.substring(0, 1).toUpperCase()}${pokemon.name.substring(1)}',
+              style: const TextStyle(
+                  fontSize: 36,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+}
+
+class ContainerStats extends StatelessWidget {
+  const ContainerStats({
+    super.key,
+    required this.pokemon,
+  });
+
+  final Pokemon pokemon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: SizedBox(
+        height: 260,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey.shade300,
+                          ),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(
+                              // Primer contenedor
+                              child: Container(
+                                height: 50,
+                                decoration: const BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(6),
+                                  ),
+                                ),
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  'Ataque',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              // Segundo contenedor
+                              child: Text(
+                                pokemon.stats[1].baseStat.toString(),
+                                style: TextStyle(
+                                    color: Colors.grey.shade500,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      // SizedBox(width: 6), // Espacio horizontal de 20 unidades
+                      Container(
+                        height: 50,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey.shade300,
+                          ),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(
+                              // Primer contenedor
+                              child: Container(
+                                height: 50,
+                                decoration: const BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(6),
+                                  ),
+                                ),
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  'Ataque',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              // Segundo contenedor
+                              child: Text(
+                                pokemon.stats[1].baseStat.toString(),
+                                style: TextStyle(
+                                    color: Colors.grey.shade500,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  // const SizedBox(height: 12),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        height: 50,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey.shade300,
+                          ),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Expanded(
+                              // Primer contenedor
+                              child: Container(
+                                height: 50,
+                                decoration: const BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(6),
+                                  ),
+                                ),
+                                alignment: Alignment.center,
+                                child: const Text(
+                                  'Ataque',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              // Segundo contenedor
+                              child: Text(
+                                pokemon.stats[1].baseStat.toString(),
+                                style: TextStyle(
+                                    color: Colors.grey.shade500,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 50,
+                        width: 140,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey.shade300,
+                          ),
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(6),
+                          ),
+                        ),
+                        child: Center(
+                          child: Text('Tipo: ${pokemon.stats[2].baseStat}'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all(Colors.grey.shade800),
+                  minimumSize: MaterialStateProperty.all(
+                      const Size(double.infinity, 50)),
+                  shape: MaterialStateProperty.all(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  'Yo te elijo!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
